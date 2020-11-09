26a1961808dfd7343b6f3fef2efd490c3c901d70
seanhardison1 <seanhardison@gmail.com>
2020-11-09 15:14:42 -0500

    initial commit

---
+++ R/sfc_as_cols.R
@@ -0,0 +1,19 @@
+# A function to convert sfc to lat/lon columns within a data.frame. Removes geometry
+# Source: https://github.com/r-spatial/sf/issues/231
+
+library(sf)
+
+sfc_as_cols <- function(x, geometry, names = c("x","y")) {
+  if (missing(geometry)) {
+    geometry <- sf::st_geometry(x)
+  } else {
+    geometry <- rlang::eval_tidy(enquo(geometry), x)
+  }
+  stopifnot(inherits(x,"sf") && inherits(geometry,"sfc_POINT"))
+  ret <- sf::st_coordinates(geometry)
+  ret <- tibble::as_tibble(ret)
+  stopifnot(length(names) == ncol(ret))
+  x <- x[ , !names(x) %in% names]
+  ret <- setNames(ret,names)
+  dplyr::bind_cols(x,ret)
+}
