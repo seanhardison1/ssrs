059104a76d7d27a6a59e085db9d7fa2829bb09f8
seanhardison1 <seanhardison@gmail.com>
2020-11-23 09:41:30 -0500

    MODIS data processing

---
+++ R/bind_water_qual_and_modis.R
@@ -0,0 +1,64 @@
+library(tidyverse)
+library(sf)
+library(raster)
+library(mgcv)
+library(anytime)
+library(glmmTMB)
+library(lubridate)
+library(ggeffects)
+source(here::here("R/sfc_as_cols.R"))
+
+# read water quality data
+cb_wq <- read.csv(here::here("data/WaterQualityWaterQualityCBSeg2003.csv"))
+
+# load modis reflectance time series
+load(here::here("data/modis_reflectance_ts.rdata"))
+
+# pts <- cb_wq %>% 
+#   group_by(Station, Latitude, Longitude) %>% 
+#   dplyr::summarise(n = n()) %>% 
+#   arrange(desc(n)) %>%
+#   as.data.frame() %>% 
+#   dplyr::slice(1:40) %>% 
+#   st_as_sf(coords = c("Longitude","Latitude"), crs = 4326)
+# write_sf(pts, here::here("data/station_locs2.shp"))
+
+raw <- modis_reflec %>% 
+  mutate(Longitude = Longitude*-1,
+         datetime = as.Date(datetime, format = "%m/%d/%Y")) %>% 
+  st_as_sf(., coords = c("Longitude", "Latitude"), crs = 4326) %>% 
+  st_transform(.,"+init=epsg:3857") %>% 
+  sfc_as_cols(.,names = c("Longitude","Latitude")) %>%
+  st_set_geometry(NULL)
+
+unique_refs <- modis_reflec %>% 
+  dplyr::select(1,2) %>% 
+  distinct() %>% 
+  mutate(Longitude = Longitude*-1) %>% 
+  st_as_sf(., coords = c("Longitude", "Latitude"), crs = 4326) %>% 
+  st_transform(.,"+init=epsg:3857")
+
+cb_wq_modis <- cb_wq %>% 
+  mutate(Longitude = round(Longitude, 4),
+         Latitude = round(Latitude, 4),
+         datetime = as.Date(SampleDate, format = "%m/%d/%Y")) %>% 
+  filter(Parameter %in% c("TSS", "SALINITY"),
+         Depth < 4) %>%
+  # shrink data
+  dplyr::select(Station, datetime, Longitude, Latitude, Parameter, 
+                MeasureValue, SampleDate, sal_prof = CBSeg2003, Depth, SampleReplicateType, Layer) %>%
+  
+  # pivot variables to columns
+  pivot_wider(., names_from = Parameter, values_from = MeasureValue, values_fn = mean) %>% 
+  filter(!is.na(Longitude)) %>% 
+  st_as_sf(., coords = c("Longitude", "Latitude"), crs = 4326) %>%
+  st_transform(.,"+init=epsg:3857") %>% 
+  st_snap(.,unique_refs, tolerance = 1000) %>% 
+  sfc_as_cols(.,names = c("Longitude","Latitude")) %>%
+  st_set_geometry(NULL) %>% 
+  left_join(.,raw, by = c("datetime","Longitude","Latitude")) %>% 
+  filter(!is.na(vals)) %>% 
+  group_by(sal_prof, Station, Longitude, Latitude, datetime, vals) %>% 
+  dplyr::summarise(daily_TSS = mean(TSS, na.rm = T),
+                   daily_salinity = mean(SALINITY, na.rm = T)) %>% 
+  filter(!is.na(daily_TSS))
---
+++ R/parsing_xml.R
@@ -0,0 +1,41 @@
+library(XML)
+library(anytime)
+library(magrittr)
+library(tidyverse)
+
+g1 <- xmlParse(file = here::here("data/modis_rrs_645_2000_2010.kml"))
+g2 <- xmlParse(file = here::here("data/modis_rrs_645_2010_2020.kml"))
+xml_data_1 <- xmlToList(g1) 
+xml_data_2 <- xmlToList(g2) 
+
+# parsing XML
+modis_reflec <- NULL
+for (j in c(1,2)){
+  message(j)
+  xml_data <- get(paste0("xml_data_",j))
+  
+  for (i in 1:length(xml_data$Document)){
+    unlisted <- unlist(xml_data$Document[i]$Placemark)
+    
+    locs <- unlisted[names(unlisted) == "ExtendedData.Data..attrs.name"]
+    locs <- locs[c(-1, -(length(locs)), (-(length(locs)) + 1))]
+    img_df <- tibble(loc = locs) %>% 
+      separate(.,col = loc, into = c("Longitude","Latitude"), sep = "_", remove = T) %>% 
+      mutate(Longitude = as.numeric(str_remove(Longitude, "X\\."))/-10000,
+             Latitude = as.numeric(Latitude)/10000)
+    
+    vals <- unlisted[names(unlisted) == "ExtendedData.Data.value"]
+    vals <- as.numeric(vals[c(-1, -(length(vals)), (-(length(vals)) + 1))])
+    
+    dt <- anytime(as.numeric(unlisted[length(unlisted) - 2])/1000)
+    
+    img_df %<>% 
+      mutate(vals = vals,
+             datetime = dt)
+    
+    assign("modis_reflec", rbind(modis_reflec, img_df))
+  }
+}
+
+
+save(modis_reflec, file = here::here("data/modis_reflectance_ts.rdata"))
